3c60cfa5f3fc890da59e3a7fb0b2cf7ec4ec1a14
DanRoSi <nestor.rodriguez07@uptc.edu.co>
2024-06-01 11:37:22 -0500

    Primer Commit

---
+++ src/main/java/uptc/edu/co/proyecto_i/HelloController.java
@@ -0,0 +1,14 @@
+package uptc.edu.co.proyecto_i;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
